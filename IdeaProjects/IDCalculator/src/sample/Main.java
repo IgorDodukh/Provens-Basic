99f628178a23b2e9aca4aa3ed35cd18ef0c7b41d
IgorDodukh <ihor.dodukh@gmail.com>
2016-05-29 21:39:54 +0300

    UI changes

--- IdeaProjects/IDCalculator/src/sample/Main.java
+++ IdeaProjects/IDCalculator/src/sample/Main.java
@@ -8,15 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("It's a Calculator!");
+        primaryStage.setTitle("It's Calculator!");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-    }
 
+    }
 
     public static void main(String[] args) {
         launch(args);
